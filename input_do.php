0117e8bc165a28a6ffaa07bfafc933c26e7cdb69
YutaIto <n88.ito.yuta@gmail.com>
2021-12-11 19:55:58 +0900

    first commit

---
+++ input_do.php
@@ -0,0 +1,18 @@
+<?php
+$memo = filter_input(INPUT_POST, 'memo', FILTER_SANITIZE_SPECIAL_CHARS);
+
+require('dbconnect.php');
+$stmt = $db->prepare('insert into memos(memo) values(?)');
+if (!$stmt):
+    die($db->error);
+endif;
+$stmt->bind_param('s', $memo);
+$ret = $stmt->execute();
+
+if ($ret):
+    echo '登録されました';
+    echo '<br>→ <a href="index.php">トップへ戻る</a>';
+else:
+    $db->error;
+endif;
+?>
